24fe1636f66e910cff4a54f6b18610566d9129dc
@Bean("lee, soobeen") <02ggang9@gmail.com>
2025-04-02 20:41:12 +0900

    [Silver V] Title: 집합, Time: 1032 ms, Memory: 299136 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.kt
+++ 백준/Silver/11723. 집합/집합.kt
@@ -3,24 +3,22 @@ import java.io.*
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
-    
+
     val n = br.readLine().toInt()
     var bitMask = 0
-    val resultBuilder = StringBuilder()
 
     repeat(n) {
         val line = br.readLine().split(" ")
         when(line[0]) {
             "add" -> bitMask = bitMask.or(1 shl (line[1].toInt() - 1))
             "remove" -> bitMask = bitMask.and((1 shl (line[1].toInt() - 1)).inv())
-            "check" -> if (bitMask.and(1 shl (line[1].toInt() - 1)) > 0) resultBuilder.append(1).append("\n") else resultBuilder.append(0).append("\n")
+            "check" -> if (bitMask.and(1 shl (line[1].toInt() - 1)) > 0) bw.write("1\n") else bw.write("0\n")
             "toggle" -> bitMask = bitMask.xor(1 shl (line[1].toInt() - 1))
             "all" -> bitMask = 0.or((1 shl 20) - 1)
             "empty" -> bitMask = 0
         }
     }
 
-    bw.write(resultBuilder.toString())
     bw.flush()
     bw.close()
 }
