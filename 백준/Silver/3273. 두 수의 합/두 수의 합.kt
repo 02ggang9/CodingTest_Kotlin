81ac9082f421a5d2f2c2fc5e6fe40253f857b970
@Bean("lee, soobeen") <02ggang9@gmail.com>
2025-12-02 15:27:42 +0900

    [Silver III] Title: 두 수의 합, Time: 348 ms, Memory: 37444 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.kt
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.kt
@@ -1,20 +1,15 @@
-import java.util.LinkedHashSet
-
 fun main() {
     System.`in`.bufferedReader().use {
         val n = it.readLine().toInt()
-        val scores = it.readLine().split(" ").map { it.toInt() }
+        val scores = it.readLine().split(" ").map { it.toInt() }.toHashSet()
         val sum = it.readLine().toInt()
-        val map = LinkedHashSet<Int>(n)
         var result = 0
-
-        scores.forEach { map.add(it) }
 
         scores.forEach {
             val currentNumber = it
             val findNumber = sum - currentNumber
 
-            if (findNumber in map) result++
+            if (findNumber in scores) result++
         }
 
         println(result / 2)
