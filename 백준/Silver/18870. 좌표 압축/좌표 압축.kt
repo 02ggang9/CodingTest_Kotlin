e48aa186e11a9256170a2d5c371f08443668862e
@Bean("lee, soobeen") <02ggang9@gmail.com>
2025-12-10 09:45:45 +0900

    [Silver II] Title: 좌표 압축, Time: 1936 ms, Memory: 346968 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.kt
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.kt
@@ -2,19 +2,21 @@ import java.util.*
 
 fun main() {
     val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
     val line1 = StringTokenizer(br.readLine())
     val line2 = StringTokenizer(br.readLine())
+
     val n = line1.nextToken().toInt()
     val xPoints = IntArray(n)
 
     repeat(n) { xPoints[it] = line2.nextToken().toInt() }
 
-    val sortedXPoints = xPoints.toSet().toIntArray().sortedWith(compareBy({ it }))
+    val sortedXPoints = xPoints.toSortedSet().toIntArray()
     val hashMap = HashMap<Int, Int>(sortedXPoints.size)
     sortedXPoints.forEachIndexed { index, value -> hashMap[value] = index }
 
-    val result = StringBuilder()
-    xPoints.forEach { result.append(hashMap[it]).append(" ") }
+    xPoints.forEach { bw.write("${hashMap[it]} ") }
 
-    println(result.toString())
+    bw.flush()
+    bw.close()
 }
